31748b143bda0a492ed6f3d71d0ef81520212064
romanb123 <romanb123@users.noreply.github.com>
2019-02-22 22:56:38 +0200

    upd1

--- bootstrap-4.3.1-dist/js/to_do.js
+++ bootstrap-4.3.1-dist/js/to_do.js
@@ -1,5 +1,15 @@
 
 // function to creat a div:
 function divcreate(){
-    document.getElementById("missionsrow").innerHTML+="<div class='col-sm-12 col-md-12  col-lg-3 mission'>"+"hjhjh"+"/<div>";
+    // get the value of the missions:
+var mission=document.getElementById('mission_text').value;
+var date=document.getElementById('mission_date').value;
+var time=document.getElementById('mission_time').value;
+
+
+    // create a mission with the values:
+    document.getElementById("missionsrow").innerHTML+="<div class='col-sm-12 col-md-12  col-lg-3 mission'>"
+    +"<button class='close' class='close' aria-label='Close'>"+"<span aria-hidden='true' class='remove'>&times;</span>"
+    +"</button>"+"<h1>"+"Mission"+"</h1>"+"<p>"+mission+"</p>"+"<span class='date_time'>"+date+"<br>"+time+"</span>"
+    +"</div>";
 }
