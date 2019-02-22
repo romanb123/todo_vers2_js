b47c7c40c3718a70e526df7e0e851ae0b305d32f
romanb123 <romanb123@users.noreply.github.com>
2019-02-22 21:45:52 +0200

    up

---
+++ bootstrap-4.3.1-dist/js/to_do.js
@@ -0,0 +1,5 @@
+
+// function to creat a div:
+function divcreate(){
+    document.getElementById("missionsrow").innerHTML+="<div class='col-sm-12 col-md-12  col-lg-3 mission'>"+"hjhjh"+"/<div>";
+}
