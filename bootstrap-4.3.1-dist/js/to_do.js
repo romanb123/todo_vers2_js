c5f19e24fffd17adeb4c87d897903bf4cf0cdbba
romanb123 <romanb123@users.noreply.github.com>
2019-02-23 22:05:19 +0200

    up

--- bootstrap-4.3.1-dist/js/to_do.js
+++ bootstrap-4.3.1-dist/js/to_do.js
@@ -8,11 +8,13 @@ var time=document.getElementById('mission_time').value;
 
 
     // create a mission with the values:
-    document.getElementById("missionsrow").innerHTML+="<div class='col-sm-12 col-md-12  col-lg-3 mission'>"
+  var newdiv="<div class='col-sm-12 col-md-12  col-lg-3 mission'>"
     +"<button class='close' class='close' aria-label='Close'onclick='removemission()'>"+"<span aria-hidden='true' class='remove' >&times;</span>"
     +"</button>"+"<h1>"+"Mission"+"</h1>"+"<p>"+mission+"</p>"+"<span class='date_time'>"+date+"<br>"+time+"</span>"
-    +"</div>";
+    +"</div>"; 
+    document.getElementById("missionsrow").innerHTML+=newdiv;
 }
+
 
 
 // function to deleate a div:
