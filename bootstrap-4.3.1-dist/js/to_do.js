d59a2133d0efb00063767abb6d1508d72010401f
romanb123 <romanb123@users.noreply.github.com>
2019-03-02 13:03:31 +0200

    locstorg

--- bootstrap-4.3.1-dist/js/to_do.js
+++ bootstrap-4.3.1-dist/js/to_do.js
@@ -1,26 +1,45 @@
 
+
+
+var ArrMissions = [];
 // function to creat a div:
-function divcreate(){
-    // get the value of the missions:
-var mission=document.getElementById('mission_text').value;
-var date=document.getElementById('mission_date').value;
-var time=document.getElementById('mission_time').value;
+function divcreate() {
+  // get the value of the missions:
+  var mission = document.getElementById('mission_text').value;
+  var date = document.getElementById('mission_date').value;
+  var time = document.getElementById('mission_time').value;
+  var time = document.getElementById('mission_time').value;
 
+// to local storage:
+      var ObjMissions = {text:mission, when:date, clock:time};
+      myJSON = JSON.stringify(ObjMissions);
+    ArrMissions.push(myJSON);
+    document.getElementById("heading").innerHTML = ArrMissions;
+    localStorage.setItem("lastname",ArrMissions);
 
-    // create a mission with the values:
-  var newdiv="<div class='col-sm-12 col-md-12  col-lg-3 mission'>"
-    +"<button class='close' class='close' aria-label='Close'onclick='removemission()'>"+"<span aria-hidden='true' class='remove' >&times;</span>"
-    +"</button>"+"<h1>"+"Mission"+"</h1>"+"<p>"+mission+"</p>"+"<span class='date_time'>"+date+"<br>"+time+"</span>"
-    +"</div>"; 
-    document.getElementById("missionsrow").innerHTML+=newdiv;
+  var newdiv = "<div class='col-sm-12 col-md-12  col-lg-3 mission'>"
+    + "<button class='close' class='close' aria-label='Close'onclick='removemission()'>" + "<span aria-hidden='true' class='remove' >&times;</span>"
+    + "</button>" + "<h1>" + "Mission" + "</h1>" + "<p>" + mission + "</p>" + "<span class='date_time'>" + date + "<br>" + time + "</span>"
+    + "</div>";
+  document.getElementById("missionsrow").innerHTML += newdiv;
 }
 
-
-
 // function to deleate a div:
-function removemission(){
-var d=event.target;
-var p=d.parentElement;
-var s=p.parentElement;
-s.parentElement.removeChild(s);
-};
+function removemission() {
+  var d = event.target;
+  var p = d.parentElement;
+  var s = p.parentElement;
+  s.parentElement.removeChild(s);
+}
+
+// function to create an array
+// function newArray() {
+//   var mission = document.getElementById('mission_text').value;
+//   var date = document.getElementById('mission_date').value;
+//   var time = document.getElementById('mission_time').value;
+//     var ObjMissions = {text:mission, when:date, clock:time};
+//     myJSON = JSON.stringify(ObjMissions);
+//   ArrMissions.push(myJSON);
+//   document.getElementById("heading").innerHTML = ArrMissions;
+//   localStorage.setItem("lastname",ArrMissions);
+// }
