7154437cbfdd89bd58923afbf8415dd4c11ef532
romanb123 <romanb123@users.noreply.github.com>
2019-03-06 11:03:37 +0200

    updd

--- bootstrap-4.3.1-dist/js/to_do.js
+++ bootstrap-4.3.1-dist/js/to_do.js
@@ -1,5 +1,5 @@
 
-
+var i=0;
 
 var ArrMissions = [];
 // function to creat a div:
@@ -17,19 +17,25 @@ function divcreate() {
     localStorage.setItem("lastname",myJSON);
   
     // create the div:
-  var newdiv = "<div class='col-sm-12 col-md-12  col-lg-3 mission'>"
+  var newdiv = `<div class="col-sm-12 col-md-12  col-lg-3 mission" id="${i}">`
     + "<button class='close' class='close' aria-label='Close'onclick='removemission()'>" + "<span aria-hidden='true' class='remove' >&times;</span>"
     + "</button>" + "<h1>" + "Mission" + "</h1>" + "<p>" + mission + "</p>" + "<span class='date_time'>" + date + "<br>" + time + "</span>"
     + "</div>";
   document.getElementById("missionsrow").innerHTML += newdiv;
+  i++;
 }
     // to load misions:
     function loadStorage(){
-     var stored= localStorage.getItem("lastname");
-   console.log(stored);
-   obj = JSON.parse(stored);
-    document.getElementById("heading").innerHTML=obj[0].text;
-    console.log(obj);
+     
+      var mission1=localStorage.getItem("lastname"); 
+      objmissions1 = JSON.parse(mission1);
+      for (let index = 0; index < objmissions1.length; index++) {
+      var newdiv = `<div class="col-sm-12 col-md-12  col-lg-3 mission" id="${index}">`
+      + "<button class='close' class='close' aria-label='Close'onclick='removemission()'>" + "<span aria-hidden='true' class='remove' >&times;</span>"
+      + "</button>" + "<h1>" + "Mission" + "</h1>" + "<p>" + objmissions1[index].text + "</p>" + "<span class='date_time'>" +objmissions1[index].date + "<br>" + objmissions1[index].time + "</span>"
+      + "</div>";
+    document.getElementById("missionsrow").innerHTML += newdiv;
+     }
     }
 // function to deleate a div:
 function removemission() {
