3fabfa6c9163f7023d655c29b7ddbe6a33385e8a
romanb123 <romanb123@users.noreply.github.com>
2019-03-08 09:40:21 +0200

    upd

--- bootstrap-4.3.1-dist/js/to_do.js
+++ bootstrap-4.3.1-dist/js/to_do.js
@@ -3,7 +3,7 @@
 
 
 
-var i=0;
+var i = 0;
 var ArrMissions = [];
 // function to creat a div:
 function divcreate() {
@@ -11,44 +11,54 @@ function divcreate() {
   var mission = document.getElementById('mission_text').value;
   var date = document.getElementById('mission_date').value;
   var time = document.getElementById('mission_time').value;
-  
 
-// to local storage:
-    var ObjMissions = {text:mission, when:date, clock:time};
-    var mission1=localStorage.getItem("lastname"); 
-    objmissions1 = JSON.parse(mission1);
-  if(objmissions1!=null){
-console.log(objmissions1);
-objmissions1.push(ObjMissions);
- myJSON = JSON.stringify(objmissions1);
-localStorage.setItem("lastname",myJSON);
-}
-else{
+
+  // to local storage:
+  var ObjMissions = { text: mission, when: date, clock: time };
+  var mission1 = localStorage.getItem("lastname");
+  objmissions1 = JSON.parse(mission1);
+  if (objmissions1 != null) {
+    console.log(objmissions1);
+    objmissions1.push(ObjMissions);
+    myJSON = JSON.stringify(objmissions1);
+    localStorage.setItem("lastname", myJSON);
+  }
+  else {
     ArrMissions.push(ObjMissions);
-     myJSON = JSON.stringify(ArrMissions);
-    localStorage.setItem("lastname",myJSON);
-}  
-    // create the div:
-  var newdiv = `<div class="col-sm-12 col-md-12  col-lg-3 mission" id="${i}">`
+    myJSON = JSON.stringify(ArrMissions);
+    localStorage.setItem("lastname", myJSON);
+  }
+  // create the div:
+  var newdiv = `<div class="col-sm-12 col-md-12  col-lg-3 mission"id="${i}">`
     + "<button class='close' class='close' aria-label='Close'onclick='removemission()'>" + "<span aria-hidden='true' class='remove' >&times;</span>"
-    + "</button>" + "<h1>" + "Mission" + "</h1>" + "<p>" + mission + "</p>" + "<span class='date_time'>" + date + "<br>" + time + "</span>"
+    + "</button>" + "<h1>" + "Mission"+`${i}` + "</h1>" + "<p>" + mission + "</p>" + "<span class='date_time'>" + date + "<br>" + time + "</span>"
     + "</div>";
   document.getElementById("missionsrow").innerHTML += newdiv;
   i++;
 }
-    // to load misions:
-    function loadStorage(){
-      var mission1=localStorage.getItem("lastname"); 
-      objmissions1 = JSON.parse(mission1);
-      console.log(objmissions1);
-      for (let index = 0; index < objmissions1.length; index++) {
-      var newdiv = `<div class="col-sm-12 col-md-12  col-lg-3 mission" id="${index}">`
-      + "<button class='close' class='close' aria-label='Close'onclick='removemission()'>" + "<span aria-hidden='true' class='remove' >&times;</span>"
-      + "</button>" + "<h1>" + "Mission" + "</h1>" + "<p>" + objmissions1[index].text + "</p>" + "<span class='date_time'>" +objmissions1[index].date + "<br>" + objmissions1[index].time + "</span>"
-      + "</div>";
-    document.getElementById("missionsrow").innerHTML += newdiv;
-     }
+// to load misions:
+function loadStorage() {
+
+  var mission1 = localStorage.getItem("lastname");
+  objmissions1 = JSON.parse(mission1);
+ 
+  var newdiv = "";
+  console.log(objmissions1);
+  for (let index = 0; index < objmissions1.length; index++) {
+    if (objmissions1[index] == null) {
+      newdiv = `<div id="${index}"style=display:none;>`+`</div>`;
+      document.getElementById("missionsrow").innerHTML += newdiv;
     }
+    else {
+      var newdiv = `<div class="col-sm-12 col-md-12  col-lg-3 mission" id="${index}">`
+        + "<button class='close' class='close' aria-label='Close'onclick='removemission()'>" + "<span aria-hidden='true' class='remove' >&times;</span>"
+        + "</button>" + "<h1>" + "Mission"+`${index}`+ "</h1>" + "<p>" + objmissions1[index].text + "</p>" + "<span class='date_time'>" + objmissions1[index].date + "<br>" + objmissions1[index].time + "</span>"
+        + "</div>";
+      document.getElementById("missionsrow").innerHTML += newdiv; 
+      i = objmissions1.length;
+    }
+  }
+}
 // function to deleate a div:
 function removemission() {
   var d = event.target;
@@ -56,13 +66,13 @@ function removemission() {
   var s = p.parentElement;
   s.parentElement.removeChild(s);
   console.log(s.id);
-  var idnum=s.id;
-  var mission1=localStorage.getItem("lastname"); 
-    objmissions1 = JSON.parse(mission1);
-   objmissions1.splice(s.id,1);
-    console.log(objmissions1); 
-    myJSON = JSON.stringify(objmissions1);
-    localStorage.setItem("lastname",myJSON);
+  var idnum = s.id;
+  var mission1 = localStorage.getItem("lastname");
+  objmissions1 = JSON.parse(mission1);
+  delete objmissions1[idnum];
+  console.log(objmissions1);
+  myJSON = JSON.stringify(objmissions1);
+  localStorage.setItem("lastname", myJSON);
 }
 
 // function to create an array
